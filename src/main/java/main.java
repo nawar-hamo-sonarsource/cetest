75d73b0632bc832491aefbacd2785a5f61657d02
Nawar Hamo <100778766+nawar-hamo-sonarsource@users.noreply.github.com>
2023-03-24 11:53:21 +0100

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,8 +3,7 @@
 public class main {
   public static void main(String [] args) {
 
-    System.out.println("main");
-    System.out.println("main");
+
     System.out.println("main");
     System.out.println("main");
     System.out.println("main");
